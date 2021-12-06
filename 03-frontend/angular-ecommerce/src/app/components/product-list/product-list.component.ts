8134c852b89a6f41e664b8abfdf8ebbf680993aa
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-12-07 07:53:13 +0800

    Add support for product grid

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -4,8 +4,9 @@ import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
+  templateUrl: './product-list-grid.component.html',
   // templateUrl: './product-list.component.html',
-  templateUrl: './product-list-table.component.html',
+  // templateUrl: './product-list-table.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
