edd92e0d667d53f53931e154cf7eeb6b0ac26611
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-12-07 11:17:28 +0800

    Add more books and enhance application with product images

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -9,7 +9,7 @@ import { Product } from '../common/product';
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products';
+  private baseUrl = 'http://localhost:8080/api/products?size=100';
 
   constructor(private httpClient: HttpClient) { }
 
