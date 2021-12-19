144520d95d5185f0091cce609f55e3f05a6d8768
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-12-19 17:35:57 +0800

    Add search component in Angular

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
 import { Routes, RouterModule } from '@angular/router';
-import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
-import { SearchComponent } from './components/search/search.component'
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component'
+
 const routes: Routes = [
+  {path: 'search/:keyword', component: ProductListComponent},
   {path: 'category/:id/:name', component: ProductListComponent},
   {path: 'category', component: ProductListComponent},
   {path: 'products', component: ProductListComponent},
@@ -22,8 +23,7 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductCategoryMenuComponent,
-    SearchComponent
+    ProductCategoryMenuComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
