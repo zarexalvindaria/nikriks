2b4b2138b3c65d21e61b9914c4084ee27d2949d6
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-01-02 08:01:32 +0800

    Refactor CartService addToCart method

--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -20,12 +20,9 @@ export class CartService {
 
     if (this.cartItems.length > 0) {
       // find the item in the cart based on item id
-      for (let tempCartItem of this.cartItems) {
-        if (tempCartItem.id === theCartItem.id) {
-          existingCartItem = tempCartItem;
-          break;
-        }
-      }
+      existingCartItem = this.cartItems.find(
+        (tempCartItem) => tempCartItem.id === theCartItem.id
+      )!;
 
       // check if we found it
       alreadyExistsInCart = existingCartItem != undefined;
