54af6e0189eced70e49054cb2ae21dec848e2eee
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-01-04 12:37:43 +0800

    Add decrement quantity functionality

--- 03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -36,4 +36,8 @@ export class CartDetailsComponent implements OnInit {
   incrementQuantity(theCartItem: CartItem) {
     this.cartService.addToCart(theCartItem);
   }
+
+  decrementQuantity(theCartItem: CartItem) {
+    this.cartService.decrementQuantity(theCartItem);
+  }
 }
--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -6,6 +6,7 @@ import { CartItem } from '../common/cart-item';
   providedIn: 'root',
 })
 export class CartService {
+
   cartItems: CartItem[] = [];
 
   totalPrice: Subject<number> = new Subject<number>();
@@ -71,5 +72,32 @@ export class CartService {
       );
       console.log(`-----`);
     }
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    
+    theCartItem.quantity --;
+
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    }
+    else {
+      this.computeCartTotals();
+    }
+  }
+
+  remove(theCartItem: CartItem) {
+
+    //get index of item in the array
+    const itemIndex = this.cartItems.findIndex(
+      tempCartItem => tempCartItem.id === theCartItem.id);
+
+    // if found, remove the itme from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
+    
   }
 }
