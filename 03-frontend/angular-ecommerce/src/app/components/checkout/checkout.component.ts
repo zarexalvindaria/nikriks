e1c9ab59961ae3c234b4aaffe8ba9e94feb287ce
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-01-09 20:51:23 +0800

    test: Add event handler for form submission
    Add test event handler for form submission. Output is initially printed
    on the console for testing.

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -20,4 +20,12 @@ export class CheckoutComponent implements OnInit {
       }),
     });
   }
+  onSubmit() {
+    console.log('Handling the submit button');
+    console.log(this.checkoutFormGroup.get('customer')!.value);
+    console.log(
+      'The email address is ' +
+        this.checkoutFormGroup.get('customer')!.value.email
+    );
+  }
 }
