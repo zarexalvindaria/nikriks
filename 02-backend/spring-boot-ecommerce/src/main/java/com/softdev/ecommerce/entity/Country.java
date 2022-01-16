8f6e92549a205d032104125daac07edb56b28198
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-01-17 06:15:00 +0800

    feat: Develop country and region JPA entities
    Create country and region JPA entities for the backend.

--- 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/entity/Country.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/entity/Country.java
@@ -1,2 +1,28 @@
-package com.softdev.ecommerce.entity;public class Country {
+package com.softdev.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="country")
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="code")
+    private String code;
+
+    @Column(name="name")
+    private String name;
+    
+    @OneToMany(mappedBy = "country")
+    private List<Region> regions;
 }
