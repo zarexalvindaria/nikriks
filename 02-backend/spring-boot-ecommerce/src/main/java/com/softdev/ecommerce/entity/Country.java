b6729d07c80493086baf55a95cd5ac52977e972f
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-01-18 07:06:49 +0800

    feat: Create Spring Data Repo for country table
    Create Spring Data Repository for the country table

--- 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/entity/Country.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/entity/Country.java
@@ -1,5 +1,6 @@
 package com.softdev.ecommerce.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,7 +23,8 @@ public class Country {
 
     @Column(name="name")
     private String name;
-    
+
     @OneToMany(mappedBy = "country")
+    @JsonIgnore
     private List<Region> regions;
 }
