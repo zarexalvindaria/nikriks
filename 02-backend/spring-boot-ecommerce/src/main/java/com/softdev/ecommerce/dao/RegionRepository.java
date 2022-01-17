5e7fadf646b6b6a2fc7206a9b0989aa35a0f571c
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-01-18 07:21:54 +0800

    feat: Create Spring Data Repo for regions
    Create Spring Data Repository for regions table.

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/dao/RegionRepository.java
@@ -0,0 +1,15 @@
+package com.softdev.ecommerce.dao;
+
+import com.softdev.ecommerce.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
+public interface RegionRepository extends JpaRepository<Region, Integer> {
+    List<Region> findByCountryCode(@Param("code") String code);
+}
