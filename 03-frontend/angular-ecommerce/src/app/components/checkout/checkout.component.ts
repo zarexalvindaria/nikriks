6d604435fd158c8fb4be046fa335ce30b737dbed
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-01-25 02:43:49 +0800

    feat: Add event handler for checkout component
    Add handler to populate only the region based on selected country.

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Country } from 'src/app/common/country';
+import { Region } from 'src/app/common/region';
 import { PetfluencerzFormService } from 'src/app/services/nikriks-form.service';
 
 @Component({
@@ -17,6 +18,9 @@ export class CheckoutComponent implements OnInit {
   creditCardMonths: number[] = [];
 
   countries: Country[] = [];
+
+  shippingAddressRegions: Region[] = [];
+  billingAddressRegions: Region[] = [];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -124,5 +128,20 @@ export class CheckoutComponent implements OnInit {
         console.log('Retrieved credit card months: ' + JSON.stringify(data));
         this.creditCardMonths = data;
       });
+  }
+
+  getRegions(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+
+    console.log(`{formGroupName} country code: ${countryCode}`);
+    console.log(`{formGroupName} country name: ${countryName}`);
+
+    this.petFluencerzFormService.getRegions(countryCode).subscribe((data) => {
+      if (formGroupName === `shippingAddress`) {
+      }
+    });
   }
 }
