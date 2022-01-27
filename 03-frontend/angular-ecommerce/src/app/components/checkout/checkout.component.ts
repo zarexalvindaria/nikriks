610ae123d90a7ee29f90e61022a16605ebb686e6
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-01-28 04:45:40 +0800

    feat: Define Getter methods to access form controls

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -99,6 +99,16 @@ export class CheckoutComponent implements OnInit {
     });
   }
 
+  get firstName() {
+    return this.checkoutFormGroup.get('customer.firstName');
+  }
+  get lastName() {
+    return this.checkoutFormGroup.get('customer.lastName');
+  }
+  get email() {
+    return this.checkoutFormGroup.get('customer.email');
+  }
+
   copyShippingAddressToBillingAddress(event: any) {
     if (event.target.checked) {
       this.checkoutFormGroup.controls['billingAddress'].setValue(
