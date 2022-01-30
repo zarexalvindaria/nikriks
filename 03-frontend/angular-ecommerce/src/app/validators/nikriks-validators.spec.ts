61ec6b762e57771c42de4dc1a35dae19dd392570
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-01-30 22:46:45 +0800

    feat: Define custom validator rule
    Define custom validator rule starting with the whitespace.

---
+++ 03-frontend/angular-ecommerce/src/app/validators/nikriks-validators.spec.ts
@@ -0,0 +1,7 @@
+import { NikriksValidators } from './nikriks-validators';
+
+describe('NikriksValidators', () => {
+  it('should create an instance', () => {
+    expect(new NikriksValidators()).toBeTruthy();
+  });
+});
