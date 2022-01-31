dd459c7fe855f882b34f0799033b9a9983d562d4
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-01 00:38:51 +0800

    feat: Create Customer repository
    Create customer repository under dao of the Spring boot app.

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/dao/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.softdev.ecommerce.dao;
+
+import com.softdev.ecommerce.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
--- 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/dto/PurchaseResponse.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/dto/PurchaseResponse.java
@@ -6,5 +6,5 @@ import lombok.Data;
 public class PurchaseResponse {
 
     private String orderTrackingNumber;
-    
+
 }
