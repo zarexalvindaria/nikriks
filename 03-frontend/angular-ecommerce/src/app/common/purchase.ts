84a24641dfabcb82ae3aad45ae4ad354226a01c0
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-02 02:50:07 +0800

    chore: Define attraibutes for the purchase class
    Define the attributes required for the purchase class.

--- 03-frontend/angular-ecommerce/src/app/common/purchase.ts
+++ 03-frontend/angular-ecommerce/src/app/common/purchase.ts
@@ -1,2 +1,11 @@
+import { Address } from './address';
+import { Customer } from './customer';
+import { Order } from './order';
+import { OrderItem } from './order-item';
+
 export class Purchase {
+  customer?: Customer;
+  shippingAddress?: Address;
+  order?: Order;
+  orderItems?: OrderItem[];
 }
