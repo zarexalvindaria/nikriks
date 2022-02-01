3bd258be1373db5bde3326d1e296fa79de88d558
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-01 22:44:39 +0800

    chore: Create order and order items entities
    Create JPA entities for order and order item tables.

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/entity/Order.java
@@ -0,0 +1,41 @@
+package com.softdev.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="orders")
+@Getter
+@Setter
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="order_tracking_number")
+    private String orderTrackingNumber;
+
+    @Column(name="total_quantity")
+    private int totalQuantity;
+
+    @Column(name="total_price")
+    private BigDecimal totalPrice;
+
+    @Column(name="status")
+    private String status;
+
+    @Column(name="date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name="last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+}
