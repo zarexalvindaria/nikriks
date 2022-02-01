72f9ce5d5ae4b207798b18c272d67aeaad993364
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-01 22:30:40 +0800

    chore: Create address entity
    Create address JPA entity.

--- 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/entity/Address.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/entity/Address.java
@@ -1,2 +1,30 @@
-package com.softdev.ecommerce.entity;public class Address {
+package com.softdev.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="address")
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="street")
+    private String street;
+
+    @Column(name="region")
+    private String region;
+
+    @Column(name="country")
+    private String country;
+
+    @Column(name="zip_code")
+    private String zipCode;
 }
