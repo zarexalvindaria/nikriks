d8494958adbc2104bc6c714a762a39085a7b1da7
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-05 04:08:50 +0800

    feat: Develop login status component
    Develop login status component for login/logout buttons.

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { CartDetailsComponent } from './components/cart-details/cart-details.com
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
+import { LoginStatusComponent } from './components/login-status/login-status.component';
 
 const routes: Routes = [
   { path: 'checkout', component: CheckoutComponent },
@@ -41,6 +42,7 @@ const routes: Routes = [
     CartDetailsComponent,
     CheckoutComponent,
     LoginComponent,
+    LoginStatusComponent,
   ],
   imports: [
     RouterModule.forRoot(routes),
---
+++ 03-frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthStateService } from '@okta/okta-angular';
+
+import { OktaAuth } from '@okta/okta-auth-js';
+
+@Component({
+  selector: 'app-login-status',
+  templateUrl: './login-status.component.html',
+  styleUrls: ['./login-status.component.css'],
+})
+export class LoginStatusComponent implements OnInit {
+  isAuthenticated?: boolean = false;
+  userFullName?: string;
+
+  constructor(
+    private oktaAuthService: OktaAuthStateService,
+    private oktaAuth: OktaAuth
+  ) {}
+
+  ngOnInit(): void {
+    // Subscribe to authentitcation state changes
+    this.oktaAuthService.authState$.subscribe((result) => {
+      this.isAuthenticated = result.isAuthenticated;
+      this.getUserDetails();
+    });
+  }
+  getUserDetails() {
+    if (this.isAuthenticated) {
+      // Fetch the logged in user details (user's claims)
+      //
+      // user full name is exposed as a property name
+      this.oktaAuth.getUser().then((res) => {
+        this.userFullName = res.name;
+      });
+    }
+  }
+
+  logout() {
+    // Terminates the session with Okta and removes current tokesn.
+    this.oktaAuth.signOut();
+  }
+}
