5ff1ed04cf04c5ce9ada2146e3b46e037bbf0cfc
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-05 03:38:01 +0800

    feat: Develop login for login/logout buttons
    Develop login status component for login/logout buttons.

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { CartStatusComponent } from './components/cart-status/cart-status.compon
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: 'checkout', component: CheckoutComponent },
@@ -39,6 +40,7 @@ const routes: Routes = [
     CartStatusComponent,
     CartDetailsComponent,
     CheckoutComponent,
+    LoginComponent,
   ],
   imports: [
     RouterModule.forRoot(routes),
