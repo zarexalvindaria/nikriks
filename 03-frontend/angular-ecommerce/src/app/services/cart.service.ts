3bea40fb016290a491203a41a443abb3fca3eb02
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-08 22:26:11 +0800

    feat: Replace sessionStorage with localStorage
    Use localStorage instead of sessionStorage to persist data even after
    browser restart.

--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -11,7 +11,8 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  storage: Storage = sessionStorage;
+  //storage: Storage = sessionStorage;
+  storage: Storage = localStorage;
 
   constructor() {
     // read data from storage
