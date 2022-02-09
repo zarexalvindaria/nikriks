c2e9b8966df264e1b8056ef1c7e67893f3465420
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-09 09:31:16 +0800

    refactor: Modify Spring Data REST Detection Strategy
    Disallow viewing customer REST api endpoint on the browser.

--- 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/dao/ProductRepository.java
@@ -4,8 +4,10 @@ import com.softdev.ecommerce.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
