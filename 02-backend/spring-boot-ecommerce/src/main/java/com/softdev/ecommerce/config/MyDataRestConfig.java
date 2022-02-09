7d089f28989e7ca42a810e04bcaa65fea3ddea41
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-09 09:12:21 +0800

    refactor: Refactor backend configs
    Fix deprecated method for Spring Data Rest; configure CORS mapping for
    Spring Data REST.

--- 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/config/MyDataRestConfig.java
@@ -5,6 +5,7 @@ import com.softdev.ecommerce.entity.Product;
 import com.softdev.ecommerce.entity.ProductCategory;
 import com.softdev.ecommerce.entity.Region;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -19,6 +20,9 @@ import java.util.Set;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Value("${allowed.origins}")
+    private String[] theAllowedOrigins;
 
     private EntityManager entityManager;
 
@@ -46,6 +50,9 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         // call an internal helper method
         exposeIds(config);
+
+        // configure cors mapping
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
     }
 
     private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
--- 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/dao/CountryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/dao/CountryRepository.java
@@ -3,9 +3,7 @@ package com.softdev.ecommerce.dao;
 import com.softdev.ecommerce.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
 public interface CountryRepository extends JpaRepository<Country, Integer> {
 }
