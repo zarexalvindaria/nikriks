b2996e21fd08fb2f4a390804f013e3d67511725e
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-12 12:37:40 +0800

    feat: Secure frontend
    Create an interceptor service.

---
+++ 03-frontend/angular-ecommerce/src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,44 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
+import { request } from 'http';
+import { from, lastValueFrom, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor(private oktaAuth: OktaAuthService) {}
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return from(this.handleAccess(request, next));
+  }
+  private async handleAccess(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Promise<HttpEvent<any>> {
+    // Only ad an access token for secured endpoints
+    const securedEndpoints = ['http://localhost:8080/api/orders'];
+
+    if (securedEndpoints.some((url) => request.urlWithParams.includes(url))) {
+      // get access token
+      const accessToken = await this.oktaAuth.getAccessToken();
+
+      // clone the request and add new header with access token
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer' + accessToken,
+        },
+      });
+    }
+
+    return await lastValueFrom(next.handle(request));
+  }
+}
