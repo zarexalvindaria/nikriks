7c527388b81e0d403498c7de0bd4571ffe8cf8e0
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-12 14:25:06 +0800

    feat: Secure communication with HTTPS
    Update API Trusted Origins in Okta dashboard.

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oa3t1bufhALXSq3X5d7',
     issuer: 'https://dev-10747744.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
