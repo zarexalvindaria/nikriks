172b4ed9fc91b9c96e6a36a5063a73ca576bc2a1
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-13 06:47:42 +0800

    feat: Add payment processing feature
    Update CheckoutController to expose /payment-intent endpoint.

--- 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/controller/CheckoutController.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/controller/CheckoutController.java
@@ -1,9 +1,17 @@
 package com.softdev.ecommerce.controller;
 
+import com.softdev.ecommerce.dto.PaymentInfo;
 import com.softdev.ecommerce.dto.Purchase;
 import com.softdev.ecommerce.dto.PurchaseResponse;
 import com.softdev.ecommerce.service.CheckoutService;
-import org.springframework.web.bind.annotation.*;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/checkout")
@@ -21,5 +29,15 @@ public class CheckoutController {
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
         return purchaseResponse;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }
