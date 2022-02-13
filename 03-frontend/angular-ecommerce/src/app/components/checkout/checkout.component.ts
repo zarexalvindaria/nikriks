fbe2eac2235a32db7fa388d97c1be93745424df7
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-13 16:19:09 +0800

    feat: Disable purchase button on certain scenarios
    Disable purchase button to prevent multiple clicks.

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -47,6 +47,8 @@ export class CheckoutComponent implements OnInit {
   cardElement: any;
   displayError: any = '';
 
+  isDisabled: boolean = false;
+
   constructor(
     private formBuilder: FormBuilder,
     private nikRiksFormService: NikriksFormService,
@@ -353,6 +355,8 @@ export class CheckoutComponent implements OnInit {
       !this.checkoutFormGroup.invalid &&
       this.displayError.textContent === ''
     ) {
+      this.isDisabled = true;
+
       this.checkoutService
         .createPaymentIntent(this.paymentInfo)
         .subscribe((paymentIntentResponse) => {
@@ -382,6 +386,7 @@ export class CheckoutComponent implements OnInit {
                 if (result.error) {
                   // inform the customer there was an error
                   alert(`There was an error: ${result.error.message}`);
+                  this.isDisabled = false;
                 } else {
                   // call REST API via the CheckoutService
                   this.checkoutService.placeOrder(purchase).subscribe({
@@ -392,9 +397,11 @@ export class CheckoutComponent implements OnInit {
 
                       // reset cart
                       this.resetCart();
+                      this.isDisabled = false;
                     },
                     error: (err: { message: any }) => {
                       alert(`There was an error: ${err.message}`);
+                      this.isDisabled = false;
                     },
                   });
                 }
