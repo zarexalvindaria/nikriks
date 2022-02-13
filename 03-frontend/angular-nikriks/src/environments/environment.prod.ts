d2f600c89d782b4983773478c256a8966a21cc67
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-14 02:55:11 +0800

    feat: Add SSL configuration for frontend
    Configure nginx to support https or SSL.

--- 03-frontend/angular-nikriks/src/environments/environment.prod.ts
+++ 03-frontend/angular-nikriks/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  nikriksApiUrl: 'https://localhost:8443/api',
+  stripePublishableKey:
+    'pk_test_51KSTswIoD1D8f90Qws7jU4hHQEjGDeyHdSDCEPV05o0W44XpemNEzpR23Fnux9DPDIdjhpR2pwU7fPDFH73Hiq1S00AHiodzsX',
 };
