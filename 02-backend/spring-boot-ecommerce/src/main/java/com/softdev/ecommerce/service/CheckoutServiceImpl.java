7e168805f4ec4987c99cc35ab1a93fe16d2ab612
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-13 16:08:13 +0800

    feat: Add customer purchase description to Stripe
    Add description of the customer purchase in Spring boot to be passed to
    the stripe dashboard.

--- 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/service/CheckoutServiceImpl.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/softdev/ecommerce/service/CheckoutServiceImpl.java
@@ -81,6 +81,7 @@ public class CheckoutServiceImpl implements CheckoutService {
         params.put("amount", paymentInfo.getAmount());
         params.put("currency", paymentInfo.getCurrency());
         params.put("payment_method_types", paymentMethodTypes);
+        params.put("description", "NikRik's customer purchase.");
 
         return PaymentIntent.create(params);
     }
