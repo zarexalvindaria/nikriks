a3cf3d541f5bd5f8154383e28b6d41d6634b3457
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-18 00:42:50 +0800

    test: Revert frontend to non-ssl
    Revert frontend to non-ssl to enable quality testing in Katalon Studio.

--- 03-frontend/angular-nikriks/src/environments/environment.ts
+++ 03-frontend/angular-nikriks/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  nikriksApiUrl: 'https://localhost:8443/api',
+  nikriksApiUrl: 'http://localhost:8443/api',
   stripePublishableKey:
     'pk_test_51KSTswIoD1D8f90Qws7jU4hHQEjGDeyHdSDCEPV05o0W44XpemNEzpR23Fnux9DPDIdjhpR2pwU7fPDFH73Hiq1S00AHiodzsX',
 };
