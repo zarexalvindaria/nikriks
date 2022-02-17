6ab48c7fe565e1b9a9ee239a954670ad568fda7e
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-17 21:57:05 +0800

    feat: Add about page
    Create about page and add initial design.

--- 03-frontend/angular-nikriks/src/app/app.module.ts
+++ 03-frontend/angular-nikriks/src/app/app.module.ts
@@ -73,7 +73,7 @@ const routes: Routes = [
   { path: 'category/:id/:name', component: ProductListComponent },
   { path: 'category', component: ProductListComponent },
   { path: 'products', component: ProductListComponent },
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
   { path: '**', redirectTo: '/products', pathMatch: 'full' },
 ];
 
