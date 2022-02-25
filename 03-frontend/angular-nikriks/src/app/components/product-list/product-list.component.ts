45fb0e6b2fcf9ecea785949e4e682f93bc1f77ff
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-02-25 22:09:03 +0800

    feat: Set default max product count to 10
    Set the default products to show 10 10 per category.

--- 03-frontend/angular-nikriks/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-nikriks/src/app/components/product-list/product-list.component.ts
@@ -19,7 +19,7 @@ export class ProductListComponent implements OnInit {
 
   // new properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 5;
+  thePageSize: number = 10;
   theTotalElements: number = 0;
 
   previousKeyword: string = '';
