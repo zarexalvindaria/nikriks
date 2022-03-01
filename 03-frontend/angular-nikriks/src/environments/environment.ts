25e53e19ba62d13530d6e5840a8b192277808b68
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-03-01 18:13:26 +0800

    feat: Deploy backend and database to Cloud
    Deploy the backend and database to Cloud Run and Cloud SQL.

--- 03-frontend/angular-nikriks/src/environments/environment.ts
+++ 03-frontend/angular-nikriks/src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  nikriksApiUrl: 'http://localhost:8443/api',
+  // nikriksApiUrl: 'http://localhost:8443/api',
+  nikriksApiUrl:
+    'http://nikriksapp-cloud-backend-1-emomen44da-uc.a.run.app/api',
   stripePublishableKey:
     'pk_test_51KSTswIoD1D8f90Qws7jU4hHQEjGDeyHdSDCEPV05o0W44XpemNEzpR23Fnux9DPDIdjhpR2pwU7fPDFH73Hiq1S00AHiodzsX',
 };
