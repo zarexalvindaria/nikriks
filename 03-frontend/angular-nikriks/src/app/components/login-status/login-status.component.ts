1369a6b7702b47b4d8ff5a1f138fd3e32560c73b
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-03-05 09:36:36 +0800

    fix: Prevent cart status from resizing
    Prevent cart status from resizing on mobile devices.

--- 03-frontend/angular-nikriks/src/app/components/login-status/login-status.component.ts
+++ 03-frontend/angular-nikriks/src/app/components/login-status/login-status.component.ts
@@ -8,7 +8,8 @@ import { OktaAuthService } from '@okta/okta-angular';
 })
 export class LoginStatusComponent implements OnInit {
   isAuthenticated: boolean = false;
-  userFullName?: string;
+  // userFullName?: string;
+  userFirstName?: string;
 
   storage: Storage = sessionStorage;
 
@@ -28,7 +29,8 @@ export class LoginStatusComponent implements OnInit {
       //
       // user full name is exposed as a property name
       this.oktaAuthService.getUser().then((res) => {
-        this.userFullName = res.name;
+        // this.userFullName = res.name;
+        this.userFirstName = res.given_name;
 
         // retrieve the user's email from authentication response
         const theEmail = res.email;
