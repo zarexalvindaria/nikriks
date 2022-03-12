5a6a0840e1eec1b1b93fe7748a51a1d1c47166cb
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2022-03-13 02:35:52 +0800

    chores: Fix table column spacing in the Admin Dashboard
    Fix table column spacing in the Admin dashboard features to distribute
    data properly in the screen.

--- 03-frontend/angular-nikriks/src/app/components/admin-menu/admin-menu.component.ts
+++ 03-frontend/angular-nikriks/src/app/components/admin-menu/admin-menu.component.ts
@@ -10,9 +10,6 @@ export class AdminMenuComponent implements OnInit {
   userGroup?: string;
   isAdmin?: boolean;
   isAuthenticated: boolean = false;
-  userFirstName?: string;
-
-  storage: Storage = sessionStorage;
 
   constructor(private oktaAuthService: OktaAuthService) {}
 
@@ -28,23 +25,13 @@ export class AdminMenuComponent implements OnInit {
     if (this.isAuthenticated) {
       // Fetch the logged in user details (user's claims)
       this.oktaAuthService.getUser().then((res) => {
-        this.userFirstName = res.given_name;
-
         console.log(`user: ` + JSON.stringify(res));
         this.isAdmin = res['groups'].includes('Administrator');
         console.log('userGroup: ' + this.userGroup);
 
         // retrieve the user's email from authentication response
         const theEmail = res.email;
-
-        // now store the email in browser storage
-        this.storage.setItem('userEmail', JSON.stringify(theEmail));
       });
     }
-  }
-
-  logout() {
-    // Terminates the session with Okta and removes current tokens.
-    this.oktaAuthService.signOut();
   }
 }
