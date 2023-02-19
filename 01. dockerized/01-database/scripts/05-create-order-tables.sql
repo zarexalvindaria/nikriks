a4d2dd3ffad2fb39a29f919facfbf039af702663
zarexalvindaria <19774995+zarexalvindaria@users.noreply.github.com>
2023-02-20 02:27:02 +0800

    fix: Add mobile number to customer db
    Add mobile number column to the customer database.

--- 01. dockerized/01-database/scripts/05-create-order-tables.sql
+++ 01. dockerized/01-database/scripts/05-create-order-tables.sql
@@ -36,6 +36,7 @@ CREATE TABLE `customer` (
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
+  `mobile_number` varchar(13) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
